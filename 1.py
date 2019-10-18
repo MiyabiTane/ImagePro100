81de67968d8672bc5b26b84c45057d40877d2448
MiyabiTane <miyabitane@yahoo.co.jp>
2019-10-18 19:18:58 +0900

    RGB->BGR

---
+++ 1.py
@@ -0,0 +1,14 @@
+import cv2
+img=cv2.imread("imori.jpg")
+
+blue=img[:,:,0].copy()
+green=img[:,:,1].copy()
+red=img[:,:,2].copy()
+
+img[:,:,0]=red
+img[:,:,2]=blue
+
+cv2.imwrite("output1.jpg",img)
+cv2.imshow("result", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
