941687e2e04cdd8da873ae476a770497692b2ab0
MiyabiTane <miyabitane@yahoo.co.jp>
2019-11-02 16:43:27 +0900

    RGB->BGR

--- 1.py
+++ 1.py
@@ -8,7 +8,7 @@ red=img[:,:,2].copy()
 img[:,:,0]=red
 img[:,:,2]=blue
 
-cv2.imwrite("output1.jpg",img)
+cv2.imwrite("./output_image/output1.jpg",img)
 cv2.imshow("result", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
