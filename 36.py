7bc34d1d276126b7dd673424ec6376fe6fbfb913
MiyabiTane <miyabitane@yahoo.co.jp>
2019-12-03 01:34:48 +0900

    失敗

---
+++ 36.py
@@ -0,0 +1,70 @@
+import numpy as np
+import cv2
+
+def gray_scale(img):
+    out=img.copy()
+    B=img[:,:,0].copy()
+    G=img[:,:,1].copy()
+    R=img[:,:,2].copy()
+
+    Y=0.2126*R+0.7152*G+0.0722*B
+    out=Y
+    out=out.astype(np.uint8)
+    return out
+
+def f_C(u):
+    if u==0:
+        return 1/np.sqrt(2)
+    else:
+        return 1
+
+def DCT(img,T=8):
+    if len(img.shape)==3:
+        H,W,C=img.shape
+    else:
+        img=np.expand_dims(img,axis=1)
+        H,W,C=img.shape
+
+    out=np.zeros((H,W,C),dtype=np.float32)
+    step=H//T
+
+    for i in range(step):
+        for j in range(step):
+            for u in range(T):
+                for v in range(T):
+                    out=2*f_C(u)*f_C(v)*out/T
+                    for x in range(T):
+                        for y in range(T):
+                            for c in range(C):
+                                out[v,u,c]+=img[y,x,c]*np.cos((2*x+1)*u*np.pi/(2*T))*np.cos((2*y+1)*v*np.pi/(2*T))
+    return out
+
+def IDCT(img,T=8,K=8):
+    if len(img.shape)==3:
+        H,W,C=img.shape
+    else:
+        img=np.expand_dims(img,axis=1)
+        H,W,C=img.shape
+    out=np.zeros((H,W,C),dtype=np.float32)
+    step=H//T
+    for i in range(step):
+        for j in range(step):
+            for x in range(T):
+                for y in range(T):
+                    for u in range(T):
+                        for v in range(T):
+                            for c in range(C):
+                                out[y,x,c]+=f_C(u)*f_C(v)*img[v,u,c]*np.cos((2*x+1)*u*np.pi/(2*T))*np.cos((2*y+1)*v*np.pi/(2*T))
+    out=out*2/T
+    out=np.clip(out,0,255)
+    out=out.astype(np.uint8)
+    return out
+
+img=cv2.imread("./input_image/imori.jpg").astype(np.float32)
+gray_img=gray_scale(img)
+img_1=DCT(gray_img)
+img_2=IDCT(img_1)
+cv2.imwrite("./output_image/output36.jpg",img_2)
+cv2.imshow("result",img_2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
