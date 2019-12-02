daf55fd885b2d3d60f12012f9502f649fd484513
MiyabiTane <miyabitane@yahoo.co.jp>
2019-12-03 02:00:11 +0900

    JPEG圧縮(Step1)離散コサイン変換

--- 36.py
+++ 36.py
@@ -26,18 +26,18 @@ def DCT(img,T=8):
         H,W,C=img.shape
 
     out=np.zeros((H,W,C),dtype=np.float32)
-    step=H//T
+    #step=H//T
 
-    for i in range(step):
-        for j in range(step):
+    for i in range(0,H,T):
+        for j in range(0,W,T):
             for u in range(T):
                 for v in range(T):
-                    out=2*f_C(u)*f_C(v)*out/T
                     for x in range(T):
                         for y in range(T):
                             for c in range(C):
-                                out[v,u,c]+=img[y,x,c]*np.cos((2*x+1)*u*np.pi/(2*T))*np.cos((2*y+1)*v*np.pi/(2*T))
+                                out[v+j,u+i,c]+=(2*f_C(u)*f_C(v)/T)*img[y+j,x+i,c]*np.cos((2*x+1)*u*np.pi/(2*T))*np.cos((2*y+1)*v*np.pi/(2*T))
     return out
+
 
 def IDCT(img,T=8,K=8):
     if len(img.shape)==3:
@@ -46,23 +46,23 @@ def IDCT(img,T=8,K=8):
         img=np.expand_dims(img,axis=1)
         H,W,C=img.shape
     out=np.zeros((H,W,C),dtype=np.float32)
-    step=H//T
-    for i in range(step):
-        for j in range(step):
+    #step=H//T
+    for i in range(0,H,T):
+        for j in range(0,W,T):
             for x in range(T):
                 for y in range(T):
-                    for u in range(T):
-                        for v in range(T):
+                    for u in range(K):
+                        for v in range(K):
                             for c in range(C):
-                                out[y,x,c]+=f_C(u)*f_C(v)*img[v,u,c]*np.cos((2*x+1)*u*np.pi/(2*T))*np.cos((2*y+1)*v*np.pi/(2*T))
+                                out[y+j,x+i,c]+=f_C(u)*f_C(v)*img[v+j,u+i,c]*np.cos((2*x+1)*u*np.pi/(2*T))*np.cos((2*y+1)*v*np.pi/(2*T))
     out=out*2/T
     out=np.clip(out,0,255)
     out=out.astype(np.uint8)
     return out
 
 img=cv2.imread("./input_image/imori.jpg").astype(np.float32)
-gray_img=gray_scale(img)
-img_1=DCT(gray_img)
+#gray_img=gray_scale(img)
+img_1=DCT(img)
 img_2=IDCT(img_1)
 cv2.imwrite("./output_image/output36.jpg",img_2)
 cv2.imshow("result",img_2)
