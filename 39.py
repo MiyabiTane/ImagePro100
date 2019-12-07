67f4878c491d2e4803a1c76ed34db19e8f82d8b4
MiyabiTane <miyabitane@yahoo.co.jp>
2019-12-07 22:13:44 +0900

    JPEG圧縮(Step.3)YCbCr表色系

--- 39.py
+++ 39.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
 
-def Ycbcr(img,a=0.7):
+def RGB_Ycbcr(img):
+    H,W,C=img.shape
+    out=np.zeros((H,W,C),dtype=np.float32)
+
     B=img[:,:,0].copy()
     G=img[:,:,1].copy()
     R=img[:,:,2].copy()
@@ -10,26 +13,39 @@ def Ycbcr(img,a=0.7):
     Cb = -0.1687 * R - 0.3313 * G + 0.5 * B + 128
     Cr = 0.5 * R - 0.4187 * G - 0.0813 * B + 128
 
-    Y*=a
+    out[:,:,0]=Y
+    out[:,:,1]=Cb
+    out[:,:,2]=Cr
+
+    return out
+
+def Ycbcr_RGB(img):
+    H,W,C=img.shape
+    out=np.zeros((H,W,C),dtype=np.float32)
+
+    Y=img[:,:,0]
+    Cb=img[:,:,1]
+    Cr=img[:,:,2]
 
     R = Y + (Cr - 128) * 1.402
     G = Y - (Cb - 128) * 0.3441 - (Cr - 128) * 0.7139
     B = Y + (Cb - 128) * 1.7718
 
-    img[:,:,0]=B
-    img[:,:,1]=G
-    img[:,:,2]=R
+    out[:,:,0]=B
+    out[:,:,1]=G
+    out[:,:,2]=R
 
-    img=np.clip(img,0,255)
-    img=img.astype(np.uint8)
-    return img
+    out=np.clip(out,0,255)
+    out=out.astype(np.uint8)
+    return out
 
-img=cv2.imread("./input_image/imori.jpg")
-cv2.imshow("original",img)
-img=img.astype(np.uint8)
-out=Ycbcr(img)
+original=cv2.imread("./input_image/imori.jpg")
+cv2.imshow("original",original)
+img=cv2.imread("./input_image/imori.jpg").astype(np.float32)
+YCbCr=RGB_Ycbcr(img)
+YCbCr[:,:,0]*=0.7
+out=Ycbcr_RGB(YCbCr)
 cv2.imwrite("./output_image/output39.jpg",out)
-cv2.imshow("original",img)
 cv2.imshow("result",out)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
