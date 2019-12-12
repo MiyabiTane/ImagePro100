21721c67fd892baa660d0a48664d2f2299fcce70
MiyabiTane <miyabitane@yahoo.co.jp>
2019-12-13 00:48:08 +0900

    Hough変換・直線検出 (Step.2) NMS

--- 45.py
+++ 45.py
@@ -1,205 +1,269 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-def Bgr2Gray(img):
-    out=img.copy()
-    B=img[:,:,0].copy()
-    G=img[:,:,1].copy()
-    R=img[:,:,2].copy()
+def Canny(img):
 
-    Y=0.2126*R+0.7152*G+0.0722*B
-    out=Y
-    out=out.astype(np.uint8)
+	# Gray scale
+	def BGR2GRAY(img):
+		b = img[:, :, 0].copy()
+		g = img[:, :, 1].copy()
+		r = img[:, :, 2].copy()
+
+		# Gray scale
+		out = 0.2126 * r + 0.7152 * g + 0.0722 * b
+		out = out.astype(np.uint8)
+
+		return out
+
+
+	# Gaussian filter for grayscale
+	def gaussian_filter(img, K_size=3, sigma=1.3):
+
+		if len(img.shape) == 3:
+			H, W, C = img.shape
+			gray = False
+		else:
+			img = np.expand_dims(img, axis=-1)
+			H, W, C = img.shape
+			gray = True
+
+		## Zero padding
+		pad = K_size // 2
+		out = np.zeros([H + pad * 2, W + pad * 2, C], dtype=np.float)
+		out[pad : pad + H, pad : pad + W] = img.copy().astype(np.float)
+
+		## prepare Kernel
+		K = np.zeros((K_size, K_size), dtype=np.float)
+		for x in range(-pad, -pad + K_size):
+			for y in range(-pad, -pad + K_size):
+				K[y + pad, x + pad] = np.exp( - (x ** 2 + y ** 2) / (2 * sigma * sigma))
+		#K /= (sigma * np.sqrt(2 * np.pi))
+		K /= (2 * np.pi * sigma * sigma)
+		K /= K.sum()
+
+		tmp = out.copy()
+
+		# filtering
+		for y in range(H):
+			for x in range(W):
+				for c in range(C):
+					out[pad + y, pad + x, c] = np.sum(K * tmp[y : y + K_size, x : x + K_size, c])
+
+		out = np.clip(out, 0, 255)
+		out = out[pad : pad + H, pad : pad + W]
+		out = out.astype(np.uint8)
+
+		if gray:
+			out = out[..., 0]
+
+		return out
+
+
+	# sobel filter
+	def sobel_filter(img, K_size=3):
+		if len(img.shape) == 3:
+			H, W, C = img.shape
+		else:
+			H, W = img.shape
+
+		# Zero padding
+		pad = K_size // 2
+		out = np.zeros((H + pad * 2, W + pad * 2), dtype=np.float)
+		out[pad : pad + H, pad : pad + W] = img.copy().astype(np.float)
+		tmp = out.copy()
+
+		out_v = out.copy()
+		out_h = out.copy()
+
+		## Sobel vertical
+		Kv = [[1., 2., 1.],[0., 0., 0.], [-1., -2., -1.]]
+		## Sobel horizontal
+		Kh = [[1., 0., -1.],[2., 0., -2.],[1., 0., -1.]]
+
+		# filtering
+		for y in range(H):
+			for x in range(W):
+				out_v[pad + y, pad + x] = np.sum(Kv * (tmp[y : y + K_size, x : x + K_size]))
+				out_h[pad + y, pad + x] = np.sum(Kh * (tmp[y : y + K_size, x : x + K_size]))
+
+		out_v = np.clip(out_v, 0, 255)
+		out_h = np.clip(out_h, 0, 255)
+
+		out_v = out_v[pad : pad + H, pad : pad + W]
+		out_v = out_v.astype(np.uint8)
+		out_h = out_h[pad : pad + H, pad : pad + W]
+		out_h = out_h.astype(np.uint8)
+
+		return out_v, out_h
+
+
+	def get_edge_angle(fx, fy):
+		# get edge strength
+		edge = np.sqrt(np.power(fx.astype(np.float32), 2) + np.power(fy.astype(np.float32), 2))
+		edge = np.clip(edge, 0, 255)
+
+		fx = np.maximum(fx, 1e-10)
+		#fx[np.abs(fx) <= 1e-5] = 1e-5
+
+		# get edge angle
+		angle = np.arctan(fy / fx)
+
+		return edge, angle
+
+
+	def angle_quantization(angle):
+		angle = angle / np.pi * 180
+		angle[angle < -22.5] = 180 + angle[angle < -22.5]
+		_angle = np.zeros_like(angle, dtype=np.uint8)
+		_angle[np.where(angle <= 22.5)] = 0
+		_angle[np.where((angle > 22.5) & (angle <= 67.5))] = 45
+		_angle[np.where((angle > 67.5) & (angle <= 112.5))] = 90
+		_angle[np.where((angle > 112.5) & (angle <= 157.5))] = 135
+
+		return _angle
+
+
+	def non_maximum_suppression(angle, edge):
+		H, W = angle.shape
+		_edge = edge.copy()
+
+		for y in range(H):
+			for x in range(W):
+					if angle[y, x] == 0:
+							dx1, dy1, dx2, dy2 = -1, 0, 1, 0
+					elif angle[y, x] == 45:
+							dx1, dy1, dx2, dy2 = -1, 1, 1, -1
+					elif angle[y, x] == 90:
+							dx1, dy1, dx2, dy2 = 0, -1, 0, 1
+					elif angle[y, x] == 135:
+							dx1, dy1, dx2, dy2 = -1, -1, 1, 1
+					if x == 0:
+							dx1 = max(dx1, 0)
+							dx2 = max(dx2, 0)
+					if x == W-1:
+							dx1 = min(dx1, 0)
+							dx2 = min(dx2, 0)
+					if y == 0:
+							dy1 = max(dy1, 0)
+							dy2 = max(dy2, 0)
+					if y == H-1:
+							dy1 = min(dy1, 0)
+							dy2 = min(dy2, 0)
+					if max(max(edge[y, x], edge[y + dy1, x + dx1]), edge[y + dy2, x + dx2]) != edge[y, x]:
+							_edge[y, x] = 0
+
+		return _edge
+
+	def hysterisis(edge, HT=100, LT=30):
+		H, W = edge.shape
+
+		# Histeresis threshold
+		edge[edge >= HT] = 255
+		edge[edge <= LT] = 0
+
+		_edge = np.zeros((H + 2, W + 2), dtype=np.float32)
+		_edge[1 : H + 1, 1 : W + 1] = edge
+
+		## 8 - Nearest neighbor
+		nn = np.array(((1., 1., 1.), (1., 0., 1.), (1., 1., 1.)), dtype=np.float32)
+
+		for y in range(1, H+2):
+				for x in range(1, W+2):
+						if _edge[y, x] < LT or _edge[y, x] > HT:
+								continue
+						if np.max(_edge[y-1:y+2, x-1:x+2] * nn) >= HT:
+								_edge[y, x] = 255
+						else:
+								_edge[y, x] = 0
+
+		edge = _edge[1:H+1, 1:W+1]
+
+		return edge
+
+	# grayscale
+	gray = BGR2GRAY(img)
+
+	# gaussian filtering
+	gaussian = gaussian_filter(gray, K_size=5, sigma=1.4)
+
+	# sobel filtering
+	fy, fx = sobel_filter(gaussian, K_size=3)
+
+	# get edge strength, angle
+	edge, angle = get_edge_angle(fx, fy)
+
+	# angle quantization
+	angle = angle_quantization(angle)
+
+	# non maximum suppression
+	edge = non_maximum_suppression(angle, edge)
+
+	# hysterisis threshold
+	out = hysterisis(edge, 100, 30)
+
+	return out
+
+
+def Hough_Line_step2(edge):
+	## Voting
+    def voting(edge):
+        H,W=edge.shape
+        rmax=np.round(np.sqrt(H**2+W**2)).astype(np.int)
+        table=np.zeros((2*rmax,180),dtype=np.int)
+        index=np.where(edge==255)
+        #zip:短い方に合わせる
+        for y,x in zip(index[0],index[1]):
+            for t in range(180):
+                the=t*np.pi/180
+                rho=np.int(x*np.cos(the)+y*np.sin(the))
+                table[rho+rmax,t]+=1
+        table=table.astype(np.uint8)
+        return table
+
+    def non_maximum_suppression(table):
+        H,W=table.shape
+        for y in range(H):
+            for x in range(180):
+                if np.max(table[max(0,y-1):min(H-1,y+2),max(0,x-1):min(179,x+2)])!=table[y,x]:
+                    table[y,x]=0
+
+        #.ravel()で１次元配列に変えてから大きい順のindex上から２０個
+        index_x=np.argsort(table.ravel())[::-1][:20]
+        index_y=index_x.copy()
+        thetas=index_x%180
+        rhos=index_y//180
+        hough=np.zeros((H,W),dtype=np.int)
+        hough[rhos,thetas]=255
+        """
+        num=np.argsort(table.ravel())[::-1][:20]
+        ind=[]
+        for n in num:
+            #[y,x]
+            ind.append([int(n%W),int(n//H)])
+        hough=np.zeros((H,W),dtype=np.int)
+        for i in ind:
+            hough[i[1],i[0]]=255
+        """
+        return hough
+
+    hough=voting(edge)
+    out=non_maximum_suppression(hough)
     return out
 
-def gaussian_filter(img,K_size=5,sigma=1.3):
-    if len(img.shape) == 3:
-        H, W, C = img.shape
-        gray = False
-    else:
-        img = np.expand_dims(img, axis=-1)
-        H, W, C = img.shape
-        gray = True
+# Read image
+img = cv2.imread("./input_image/thorino.jpg").astype(np.float32)
 
-    ## Zero padding
-    pad = K_size // 2
-    out = np.zeros([H + pad * 2, W + pad * 2, C], dtype=np.float)
-    out[pad: pad + H, pad: pad + W] = img.copy().astype(np.float)
+# Canny
+edge = Canny(img)
 
-    ## prepare Kernel
-    K = np.zeros((K_size, K_size), dtype=np.float)
-    for x in range(-pad, -pad + K_size):
-        for y in range(-pad, -pad + K_size):
-            K[y + pad, x + pad] = np.exp( - (x ** 2 + y ** 2) / (2 * (sigma ** 2)))
-    K /= (2 * np.pi * sigma * sigma)
-    K /= K.sum()
+# Hough
+out = Hough_Line_step2(edge)
 
-    tmp = out.copy()
+out = out.astype(np.uint8)
 
-    # filtering
-    for y in range(H):
-        for x in range(W):
-            for c in range(C):
-                out[pad + y, pad + x, c] = np.sum(K * tmp[y : y + K_size, x : x + K_size, c])
-
-    out = np.clip(out, 0, 255)
-    out = out[pad : pad + H, pad : pad + W]
-    #out = out.astype(np.uint8)
-
-    if gray:
-        out = out[..., 0]
-
-    return out
-
-def sobel_fil(img):
-    if len(img.shape)==3:
-        H,W,C=img.shape
-    else:
-        H,W=img.shape
-    #0パディング
-    out=np.zeros((H+2,W+2),dtype=np.float)
-    out[1:H+1,1:W+1]=img.copy().astype(np.uint8)
-    #縦方向
-    K1=[[1.,2.,1.],[0.,0.,0.],[-1.,-2.,-1.]]
-    out_tate=out.copy()
-    for h in range(1,H+1):
-        for w in range(1,W+1):
-            out_tate[h,w]=np.sum(out[h-1:h+2,w-1:w+2]*K1)
-    out_tate=np.clip(out_tate,0,255)
-    out_tate=out_tate[1:H+1,1:W+1].astype(np.uint8)
-    #横方向
-    K2=[[1.,0.,-1.],[2.,0.,-2.],[1.,0.,-1.]]
-    out_yoko=out.copy()
-    for h in range(1,H+1):
-        for w in range(1,W+1):
-            out_yoko[h,w]=np.sum(out[h-1:h+2,w-1:w+2]*K2)
-    out_yoko=np.clip(out_yoko,0,255)
-    out_yoko=out_yoko[1:H+1,1:W+1].astype(np.uint8)
-
-    return out_tate,out_yoko
-
-def make_edge(out_yoko,out_tate):
-    #これがないとうまく行かない
-    out_yoko=out_yoko.astype(np.float32)
-    out_tate=out_tate.astype(np.float32)
-    edge=np.sqrt(out_yoko**2+out_tate**2)
-    out_yoko=np.maximum(out_yoko,1e-5)
-    angle=np.arctan(out_tate/out_yoko)
-    edge=np.clip(edge,0,255)
-    return edge,angle
-
-def ryosika(angle):
-    angle=angle/np.pi*180
-    angle[angle<-22.5]=180+angle[angle<-22.5]
-    #angle=np.clip(angle,-22.5,157.5)
-    #np.where(() & ()) : ()がないと失敗する。
-    angle_out=np.zeros((angle.shape),dtype=np.uint8)
-    angle_out[np.where((-22.5<angle) & (angle<=22.5))]=0
-    angle_out[np.where((22.5<angle) & (angle<=67.5))]=45
-    angle_out[np.where((67.5<angle) & (angle<=112.5))]=90
-    angle_out[np.where((112.5<angle) & (angle<=157.5))]=135
-    return angle_out
-
-
-def NMS(angle,edge):
-    H,W=angle.shape
-    #edge=np.zeros((H+2,W+2,C),dtype=np.float32)
-    #edge[1:H+1,1:W+1]=edge_o.copy()
-    for y in range(H):
-        for x in range(W):
-            if angle[y,x]==0:
-                if max(edge[y,x],edge[y,max(0,x-1)],edge[y,min(W-1,x+1)])!=edge[y,x]:
-                    edge[y,x]=0
-            elif angle[y,x]==45:
-                if max(edge[y,x],edge[min(H-1,y+1),max(0,x-1)],edge[max(0,y-1),min(W-1,x+1)])!=edge[y,x]:
-                    edge[y,x]=0
-            elif angle[y,x]==90:
-                if max(edge[y,x],edge[max(0,y-1),x],edge[min(H-1,y+1),x])!=edge[y,x]:
-                    edge[y,x]=0
-            elif angle[y,x]==135:
-                if max(edge[y,x],edge[max(0,y-1),max(0,x-1)],edge[min(H-1,y+1),min(x+1,W-1)])!=edge[y,x]:
-                    edge[y,x]=0
-    #edge=edge[1:H+1,1:W+1]
-    return edge
-
-
-def hist(edge,HT=100,LT=30):
-    H,W=edge.shape
-    for y in range(H):
-        for x in range(W):
-            if edge[y,x]>=HT:
-                edge[y,x]=255
-            elif edge[y,x]<=LT:
-                edge[y,x]=0
-            else:
-                if len(np.where(edge[max(0,y-1):min(H-1,y+2),max(0,x-1):min(W-1,x+2)]>HT)[0])>0:
-                    edge[y,x]=255
-    return edge
-
-
-def Hough_step_1(edge):
-    H,W=edge.shape
-    rmax=np.round(np.sqrt(H**2+W**2)).astype(np.int)
-    table=np.zeros((2*rmax,180),dtype=np.int)
-    index=np.where(edge==255)
-    #zip:短い方に合わせる
-    for y,x in zip(index[0],index[1]):
-        for t in range(180):
-            the=t*np.pi/180
-            rho=np.int(x*np.cos(the)+y*np.sin(the))
-            table[rho+rmax,t]+=1
-    table=table.astype(np.uint8)
-    return table
-
-
-def Hough_step_2(table):
-    H,W=table.shape
-    for y in range(H):
-        for x in range(W):
-            if np.max(table[max(0,y-1):min(H-1,y+2),max(0,x-1):min(W-1,x+2)])!=table[y,x]:
-                table[y,x]=0
-
-    #.ravel()で１次元配列に変えてから大きい順のindex上から２０個
-    num=np.argsort(table.ravel())[::-1][:20]
-    ind=[]
-    for n in num:
-        ind.append([int(n%W),int(n//H)])
-    hough=np.zeros((H,W),dtype=np.int)
-    for i in ind:
-        hough[i[0],i[1]]=255
-
-    return hough
-
-
-img=cv2.imread("./input_image/imori.jpg").astype(np.float32)
-img_2=Bgr2Gray(img)
-img_3=gaussian_filter(img_2)
-fy,fx=sobel_fil(img_3)
-edge_o,angle_o=make_edge(fx,fy)
-angle=ryosika(angle_o)
-edge=NMS(angle,edge_o)
-edge_hist=hist(edge)
-#angle=angle.astype(np.uint8)
-#edge=edge.astype(np.uint8)
-table=Hough_step_1(edge_hist)
-#cv2.imwrite("./output_image/output42_1.jpg",angle)
-hough=Hough_step_2(table)
-hough=hough.astype(np.uint8)
-cv2.imwrite("./output_image/output44.jpg",hough)
-cv2.imshow("result1",hough)
+# Save result
+cv2.imwrite("./output_image/output45.jpg", out)
+cv2.imshow("result", out)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-"""
-np.argsort メモ
->>> import numpy as np
->>> a=np.array([[1,2,3],[4,5,6],[7,8,9]])
->>> num=np.argsort(a.ravel())[::-1][:4]
->>> num
-array([8, 7, 6, 5])
->>> num1=np.argsort(a)[:4]
->>> num1
-array([[0, 1, 2],
-       [0, 1, 2],
-       [0, 1, 2]])
-"""
