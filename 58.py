1bb62f268fd61daf6e3bf73ab87da8536738606f
MiyabiTane <miyabitane@yahoo.co.jp>
2019-12-16 11:06:31 +0900

    色分けされすぎ

--- 58.py
+++ 58.py
@@ -5,7 +5,9 @@ def labeling_4(img):
     #print("----img-----")
     #print(img[:,:,0])
     H,W,C=img.shape
-    lookup=np.zeros((H*W),dtype=np.int)
+    #lookup=np.zeros((H*W),dtype=np.int)
+    lookup=[0 for _ in range(H*W)]
+    #print("lookup={}".format(lookup))
     #init label
     label=np.zeros((H,W),dtype=np.int) #black
     label[img[:,:,0]>0]=1 #white
@@ -17,7 +19,7 @@ def labeling_4(img):
     for y in range(H):
         for x in range(W):
             if label[y,x]==0: #black
-                pass
+                continue
             c1=label[max(0,y-1),x] #上
             c2=label[y,max(0,x-1)] #左
             print("c1,c2={},{}".format(c1,c2))
@@ -41,20 +43,25 @@ def labeling_4(img):
                     print("here,4 : {},{}".format(y,x))
                     label[y,x]=c2
                     lookup[c1]=c2
-        #see lookup table
-        for l in range(2,n+1):
-            if lookup[l]!=0 and lookup[l]!=l:
-                label[l]==lookup[l]
+
+    #see lookup table
+    for l in range(2,n+1):
+        if lookup[l]!=0 and lookup[l]!=l:
+            label[l]==lookup[l]
         #print("----label==2-----")
         #print(np.where(label==2))
         #assign color
-        colors=[[234,145,152],[241,103,63],[ 51,204,204],[0,0,255]]
-        out=np.zeros((H,W,C),dtype=np.float32)
-        for i in range(2,n+1):
-            out[label==i]=colors[i-2]
+    colors=[[234,145,152],[241,103,63],[ 51,204,204],[0,0,255],[255,0,0],[0,255,0],[255,255,0]]
+    out=np.zeros((H,W,C),dtype=np.float32)
+    print(n)
+    i=2
+    while i in label:
+        print(i)
+        out[label==i]=colors[i-2]
+        i+=1
 
-        out=out.astype(np.uint8)
-        return out
+    out=out.astype(np.uint8)
+    return out
 
 img=cv2.imread("./input_image/seg.png").astype(np.float32)
 out=labeling_4(img)
