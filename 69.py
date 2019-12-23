4e4b23761d2494a18fe12b2eb6c98a40bea7c878
MiyabiTane <miyabitane@yahoo.co.jp>
2019-12-23 11:43:21 +0900

    HOG (Step.4) 特徴量の描画

--- 69.py
+++ 69.py
@@ -46,7 +46,7 @@ def make_hist(ang,N=8):
         for x in range(x_step):
             for j in range(N):
                 for i in range(N):
-                    print("y,x={},{}".format(y,x))
+                    #print("y,x={},{}".format(y,x))
                     hist[y,x,ang[y*4+j,x*4+i]]+=mag[y*4+j,x*4+i]
     #print("hist shape={}".format(hist.shape))
     return hist
@@ -61,14 +61,29 @@ def nor_hist(hist,C=3,epsilon=1):
             hist[y,x]=hist[y,x]/np.sqrt(np.sum(hist[max(y-num,0):min(y+num+1,H),max(x-num,0):min(x+num+1,W)]**2)+epsilon)
     return hist
 
-def draw_hist(ang):
-    H,W=ang.shape
-    out=np.zeros((H,W),dtype=np.float32)
-    #255:白 0:黒
-    ang=ang*255/np.max(ang)
-    for y in range(H):
-        for x in range(W):
-            out[y,x]=ang[y,x]
+def draw_hist(hist,gray,N=8):
+    H,W=gray.shape
+    h,w,C=hist.shape
+    out=gray.copy().astype(np.uint8)
+
+    for y in range(h):
+        for x in range(w):
+            center_x=x*N+N//2
+            center_y=y*N+N//2
+            #len(hist[y,x])=9
+            h=hist[y,x]/np.sum(hist[y,x])
+            h/=np.max(h)
+            #angは9分割されていた。
+            for c in range(9):
+                angle=(20*c+10)*np.pi/180
+                x_right=int(center_x+(N//2)*np.cos(angle))
+                x_left=int(center_x-(N//2)*np.cos(angle))
+                y_up=int(center_y-(N//2)*np.sin(angle))
+                y_bottom=int(center_y+(N//2)*np.sin(angle))
+                #白:255　黒:0　に変換
+                color=int(255*h[c])
+                #cv2.line(img, pt1, pt2, color, thickness=1, lineType=cv2.LINE_8, shift=0)
+                cv2.line(out,(x_left,y_up),(x_right,y_bottom),(color,color,color),thickness=1)
     return out
 
 img=cv2.imread("./input_image/imori.jpg").astype(np.float32)
@@ -76,8 +91,8 @@ gray=gray_scale(img)
 mag,ang=hog_step1(gray)
 hist=make_hist(ang)
 hist_n=nor_hist(hist)
-out=draw_hist(ang)
-out=out.astype(np.uint8)
+out=draw_hist(hist_n,gray)
+
 cv2.imshow("result",out)
 cv2.imwrite("./output_image/output69.png",out)
 cv2.waitKey(0)
