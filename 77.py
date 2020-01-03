9beaab42ee452dac203b08a10dcd073bccdf5f74
MiyabiTane <miyabitane@yahoo.co.jp>
2020-01-03 21:29:55 +0900

    ガボールフィルタ

---
+++ 77.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+def gabor(K=111,s=10,g=1.2,l=10,p=0,A=0):
+    G=np.zeros((K,K),dtype=np.float32)
+    for y in range(K):
+        for x in range(K):
+            y-=K//2;x-=K//2
+            #度→ラジアン
+            A=A*np.pi/180.
+            x_d=np.cos(A)*x+np.sin(A)*y
+            y_d=-np.sin(A)*x+np.cos(A)*y
+            y+=K//2;x+=K//2
+            G[y,x]=np.exp(-(x_d**2+(g**2)*(y_d**2))/(2*(s**2)))*np.cos(2*np.pi*x_d/(l+p))
+    #絶対値の和が１になるように正規化
+    G/=np.sum(G)
+    #0~255に正規化
+    G-=np.min(G)
+    G=G*255/np.max(G)
+    G=G.astype(np.uint8)
+    return G
+
+G=gabor()
+cv2.imwrite("./output_image/output77.jpg",G)
+cv2.imshow("result",G)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
