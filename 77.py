059b83ab09f13805da3dd193ead16c0720f0f26d
MiyabiTane <miyabitane@yahoo.co.jp>
2020-01-03 21:50:18 +0900

    ガボールフィルタ

--- 77.py
+++ 77.py
@@ -7,11 +7,11 @@ def gabor(K=111,s=10,g=1.2,l=10,p=0,A=0):
         for x in range(K):
             y-=K//2;x-=K//2
             #度→ラジアン
-            A=A*np.pi/180.
-            x_d=np.cos(A)*x+np.sin(A)*y
-            y_d=-np.sin(A)*x+np.cos(A)*y
+            theta=A*np.pi/180.
+            x_d=np.cos(theta)*x+np.sin(theta)*y
+            y_d=-np.sin(theta)*x+np.cos(theta)*y
             y+=K//2;x+=K//2
-            G[y,x]=np.exp(-(x_d**2+(g**2)*(y_d**2))/(2*(s**2)))*np.cos(2*np.pi*x_d/(l+p))
+            G[y,x]=np.exp(-(x_d**2+(g**2)*(y_d**2))/(2*(s**2)))*np.cos(2*np.pi*x_d/l+p)
     #絶対値の和が１になるように正規化
     G/=np.sum(G)
     #0~255に正規化
